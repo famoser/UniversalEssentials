f3ecc04bc0d8b624793eae3bb2dbb9e8c5c938c7
Florian Moser <git@famoser.ch>
2016-03-13 22:56:48 +0100

    initial commit

---
+++ UniversalEssentials/Helpers/ResolutionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Famoser.FrameworkEssentials.Singleton;
+
+namespace UniversalEssentials.Helpers
+{
+    public class ResolutionHelper : SingletonBase<ResolutionHelper>
+    {
+        public double WidthOfDevice
+        {
+            get { return Window.Current.Bounds.Width; }
+        }
+
+        public double HeightOfDevice
+        {
+            get { return Window.Current.Bounds.Height; }
+        }
+    }
+}
