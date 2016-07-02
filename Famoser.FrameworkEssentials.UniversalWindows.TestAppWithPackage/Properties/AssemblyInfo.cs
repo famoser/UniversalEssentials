b9a75f85b30bf008db2cd55076700540621e5d9c
florianalexandermoser <git@famoser.ch>
2016-07-03 00:26:09 +0200

    trying to add xaml to essentials

--- Famoser.FrameworkEssentials.UniversalWindows.TestAppWithPackage/Properties/AssemblyInfo.cs
+++ Famoser.FrameworkEssentials.UniversalWindows.TestAppWithPackage/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodedUITestProject1")]
+[assembly: AssemblyTitle("Famoser.FrameworkEssentials.UniversalWindows.TestAppWithPackage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodedUITestProject1")]
+[assembly: AssemblyProduct("Famoser.FrameworkEssentials.UniversalWindows.TestAppWithPackage")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c548cfb4-70da-4a5d-a85f-4b0d7c6b1b32")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +23,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
